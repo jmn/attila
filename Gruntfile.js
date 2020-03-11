dcd12b9cb862c041de0c32c44bc536823749261f
Jussi Norlund <norlund@gmail.com>
2020-03-11 23:11:46 +0100

    remove highlight because I use PrismJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,6 @@ module.exports = function(grunt) {
         '<%= config.jsSrcDir %>/libs/jquery-3.4.1.min.js',
         '<%= config.jsSrcDir %>/libs/jquery.fitvids.js',
         '<%= config.jsSrcDir %>/libs/jquery.history.js',
-        '<%= config.jsSrcDir %>/libs/highlight.pack.js',
         '<%= config.jsSrcDir %>/libs/nprogress.js',
         'node_modules/ghosthunter/dist/jquery.ghosthunter.js'
       ]
